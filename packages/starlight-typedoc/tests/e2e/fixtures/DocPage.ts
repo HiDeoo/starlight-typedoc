8ee3f9f48124dd8b259e7ca401c6f71996a3f3ff
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-12-19 11:19:02 +0100

    feat!: bump minimum required Astro version to 4.0 and Starlight to 0.15.0

--- packages/starlight-typedoc/tests/e2e/fixtures/DocPage.ts
+++ packages/starlight-typedoc/tests/e2e/fixtures/DocPage.ts
@@ -27,14 +27,12 @@ export class DocPage {
   }
 
   get #sidebar() {
-    return this.page.getByRole('navigation', { name: 'Main' }).locator('div.sidebar')
+    return this.page.getByRole('navigation', { name: 'Main' })
   }
 
   get typeDocSidebarLabel() {
-    return this.#typeDocSidebarRootDetails.getByRole('heading', {
+    return this.#typeDocSidebarRootDetails.getByText(this.#expectedTypeDocSidebarLabel, {
       exact: true,
-      level: 2,
-      name: this.#expectedTypeDocSidebarLabel,
     })
   }
 
@@ -45,7 +43,7 @@ export class DocPage {
   get #typeDocSidebarRootDetails() {
     return this.#sidebar
       .getByRole('listitem')
-      .locator(`details:has(summary > h2:has-text("${this.#expectedTypeDocSidebarLabel}"))`)
+      .locator(`details:has(summary > div > span:has-text("${this.#expectedTypeDocSidebarLabel}"))`)
   }
 
   getTypeDocSidebarItems() {
@@ -58,7 +56,7 @@ export class DocPage {
     for (const category of await list.locator('> li > details').all()) {
       items.push({
         collapsed: !(await category.getAttribute('open')),
-        label: await category.locator(`> summary > h2`).textContent(),
+        label: await category.locator(`> summary > div > span`).textContent(),
         items: await this.#getTypeDocSidebarChildrenItems(category.locator('> ul')),
       })
     }
